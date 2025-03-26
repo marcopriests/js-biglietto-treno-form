e6d25b6dc40c5193a77e454689f64d414d5b9f8b
marcopriests <prtmrc98@gmail.com>
2025-03-26 14:28:42 +0100

    [UPD] comportamento bottone fatto

--- js/script.js
+++ js/script.js
@@ -5,16 +5,26 @@ const bottone = document.getElementById('bottone')
 
 //creo la costante che indica il prezzo base del biglietto per km
 const price = 0.21
+let ticketPrice = 0
 
 
 //comportamento bottone
 bottone.addEventListener('click', function(){
     if (age.value < 18) {
-        console.log("minorenne")
+        ticketPrice = price * km.value * 0.8
+
+        console.log("Hai diritto allo sconto minorenni. Il prezzo del biglietto è di " + ticketPrice.toFixed(2) + "€")
+
     } else if (age.value >= 65) {
-        console.log("over 65")
+        ticketPrice = price * km.value * 0.6
+
+        console.log("Hai diritto allo sconto per over 65. Il prezzo del biglietto è di " + ticketPrice.toFixed(2) + "€")
+
     } else {
-        console.log("pepperoni")
+        ticketPrice = price * km.value
+
+        console.log("Non hai accesso a sconti. Il prezzo del biglietto è di " + ticketPrice.toFixed(2) + "€")
+        
     }
 
     console.log(age.value)
